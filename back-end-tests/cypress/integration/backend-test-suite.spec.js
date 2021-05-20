21be820f294ea2ddfe9f91a26b59d40622ace7fd
Simone L <lentini.simone@gmail.com>
2021-05-20 11:29:28 +0200

    created TC02 Get rooms list

--- back-end-tests/cypress/integration/backend-test-suite.spec.js
+++ back-end-tests/cypress/integration/backend-test-suite.spec.js
@@ -1,24 +1,38 @@
 /// <reference types = "cypress" />
 
-describe('Test suite for the backend tests of Hotel site', () => {
-    it('TC01 - Login to the application', () => {
+describe("Test suite for the backend tests of Hotel site", () => {
+    it("TC01 - Login to the application", () => {
         //cy.login()
         const USER_CREDENTIALS = {
-            "username": "tester01",
-            "password": "GteteqbQQgSr88SwNExUQv2ydb7xuf8c"
-        }
+            username: "tester01",
+            password: "GteteqbQQgSr88SwNExUQv2ydb7xuf8c",
+        };
 
         cy.request({
-            method: 'POST',
-            url: 'http://localhost:3000/api/login',
+            method: "POST",
+            url: "http://localhost:3000/api/login",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify(USER_CREDENTIALS)
-        }).then((response => {
-            expect(response.status).to.eq(200)
-            Cypress.env({ loginToken: response.body })
-            cy.log(response.body)
-        }))
-    })
-})
+            body: JSON.stringify(USER_CREDENTIALS),
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            Cypress.env({ loginToken: response.body });
+            cy.log(response.body);
+        });
+    });
+
+    it("TC02 - GET Rooms list", () => {
+        cy.request({
+            method: "GET",
+            url: "http://localhost:3000/api/rooms",
+            headers: {
+                "X-User-Auth": JSON.stringify(Cypress.env().loginToken),
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            cy.log(JSON.stringify(response.body))
+        });
+    });
+});
