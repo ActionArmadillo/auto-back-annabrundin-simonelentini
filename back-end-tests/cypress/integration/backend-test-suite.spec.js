fce554ac05ab8cca8f1e4614e1a2ecc50029de86
Simone L <lentini.simone@gmail.com>
2021-05-20 12:22:47 +0200

    created TC05 Create new client

--- back-end-tests/cypress/integration/backend-test-suite.spec.js
+++ back-end-tests/cypress/integration/backend-test-suite.spec.js
@@ -35,4 +35,66 @@ describe("Test suite for the backend tests of Hotel site", () => {
             cy.log(JSON.stringify(response.body))
         });
     });
+
+    it('TCO5 - Create new client', () => {
+
+        // Get the clients list
+        cy.request({
+            method: "GET",
+            url: "http://localhost:3000/api/clients",
+            headers: {
+                "X-User-Auth": JSON.stringify(Cypress.env().loginToken),
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            cy.log(JSON.stringify(response.body))
+        });
+
+
+        //Create a client request
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/api/client/new',
+            headers: {
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "id": "",
+                "created": "",
+                "name": "Herr Gurka",
+                "email": "green@giant.com",
+                "telephone": "2525 6677 3131"
+            }
+        }).then((response => {
+            expect(response.status).to.eq(200)
+        }))
+
+        // the check last created client
+        cy.request({
+            method: "GET",
+            url: "http://localhost:3000/api/clients",
+            headers: {
+                "X-User-Auth": JSON.stringify(Cypress.env().loginToken),
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            let lastID = response.body[response.body.length - 1].id
+            cy.log(lastID)
+
+            cy.request({
+                method: 'GET',
+                url: 'http://localhost:3000/api/client/' + lastID,
+                headers: {
+                    'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                    'Content-Type': 'application/json'
+                }
+            }).then((response => {
+                expect(response.status).to.eq(200)
+                cy.log(JSON.stringify(response.body))
+            }))
+        })
+    })
 });
