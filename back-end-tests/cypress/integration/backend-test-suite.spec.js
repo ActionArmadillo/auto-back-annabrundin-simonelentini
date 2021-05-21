ebc1f2ff40d1874aed5eb4ab0d3d0f6a88845d78
ActionArmadillo <62772766+ActionArmadillo@users.noreply.github.com>
2021-05-21 23:32:47 +0200

    reservations done

--- back-end-tests/cypress/integration/backend-test-suite.spec.js
+++ back-end-tests/cypress/integration/backend-test-suite.spec.js
@@ -19,7 +19,7 @@ describe('Test suite for the backend tests of Hotel site', () => {
         cy.logout()
     });
 
-  it('TC01 - Retrieve rooms list', () => {
+    it('TC01 - Retrieve rooms list', () => {
         cy.getRooms().then((response => {
             expect(response.status).to.eq(200)
             expect(response.body).not.be.empty
@@ -94,8 +94,8 @@ describe('Test suite for the backend tests of Hotel site', () => {
             expect(response.body[response.body.length - 1].id).to.be.lessThan(3)
         }))
     })
-  
-  it("TCO5 - Create new client", () => {
+
+    it("TCO5 - Create new client", () => {
 
         // Get the clients list
         cy.getClients().then((response) => {
@@ -104,7 +104,7 @@ describe('Test suite for the backend tests of Hotel site', () => {
         });
 
         //Create a client request
-    
+
         cy.createNewClient("", "", "Herr Gurka", "green@giant.com", "2525 6677 3131")
             .then((response => {
                 expect(response.status).to.eq(200)
@@ -217,26 +217,86 @@ describe('Test suite for the backend tests of Hotel site', () => {
         cy.createNewBill("", "", 10500, false).then((response => {
             expect(response.status).to.eq(200)
             cy.log(JSON.stringify(response.body))
-          
-        // the check last created bill
-        cy.getBills().then((response) => {
-            expect(response.status).to.eq(200);
+
+            // the check last created bill
+            cy.getBills().then((response) => {
+                expect(response.status).to.eq(200);
+                cy.log(JSON.stringify(response.body))
+                let lastID = response.body[response.body.length - 1].id
+                cy.log(lastID)
+
+                // edit the last bill
+                cy.editBill(lastID, "2020-01-05T12:00:00.000Z", 10500, true).then((response => {
+                    expect(response.status).to.eq(200)
+                    cy.log(JSON.stringify(response.body))
+
+                }))
+
+            })
+
+            cy.getBills().then((response) => {
+                expect(response.status).to.eq(200);
+                cy.log(JSON.stringify(response.body))
+            })
+        }))
+    })
+
+    it('TC09 - Create a new Reservation', () => {
+
+        cy.createNewReservation("", "", "2021-05-30", "2021-06-06", 1, 1, 1).then((response => {
+            expect(response.status).to.eq(200)
+            cy.log(JSON.stringify(response.body))
+        }))
+
+        cy.getReservations().then((response => {
+            expect(response.status).to.eq(200)
             cy.log(JSON.stringify(response.body))
             let lastID = response.body[response.body.length - 1].id
-            cy.log(lastID)
-            
-            // edit the last bill
-            cy.editBill(lastID, "2020-01-05T12:00:00.000Z", 10500, true).then((response => {
-                expect(response.status).to.eq(200)
-                cy.log(JSON.stringify(response.body))
+            var reservationData = response.body[lastID - 1]
 
-            }))
+            // assert that the new room is created correctly
+            expect(reservationData.id).eq(2)
+            expect(reservationData.created).is.not.empty
+            expect(reservationData.start).to.eq("2021-05-30")
+            expect(reservationData.end).to.eq("2021-06-06")
+            expect(reservationData.client).to.eq(1)
+            expect(reservationData.room).to.eq(1)
+            expect(reservationData.bill).to.eq(1)
+        }))
 
-        })
-        
-        cy.getBills().then((response) => {
-            expect(response.status).to.eq(200);
+        /*
+        cy.getReservation(2).then((response => {
+            expect(response.status).to.eq(200)
             cy.log(JSON.stringify(response.body))
-        })
+        }))
+        */
+    })
+
+    it.only('TC03 - Edit and delete last Reservation', () => {
+        cy.getReservations().then((response => {
+            expect(response.status).to.eq(200)
+        })).then((response => {
+            let lastID = response.body[response.body.length - 1].id
+            let created = response.body[lastID - 1].created
+
+            cy.log(JSON.stringify(response.body))
+
+            cy.editRoom(lastID, created, "2021-05-25", "2021-06-30", 1, 1, 1).then((response => {
+                expect(response.status).to.eq(200)
+                cy.log(JSON.stringify(response.body[2]))
+            }))
+        }))
+
+        cy.getRooms().then((response => {
+            expect(response.status).to.eq(200)
+            let lastID = response.body[response.body.length - 1].id
+            var roomData = response.body[lastID - 1]
+
+            expect(roomData.floor).to.eq(5)
+            expect(roomData.number).to.eq(555)
+            expect(roomData.available).to.be.true
+            expect(roomData.price).to.eq(1024)
+            expect(roomData.features).to.contain("ensuite", "sea_view")
+        }))
     })
 })
